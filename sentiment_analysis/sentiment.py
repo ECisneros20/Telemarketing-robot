ac75c26a87c84eaa6cecf08478caa16c2f9df603
EnzoC <enzo.cisneros@pucp.edu.pe>
2022-02-13 14:26:08 -0500

    third commit

--- sentiment_analysis/sentiment.py
+++ sentiment_analysis/sentiment.py
@@ -1,45 +1,24 @@
-#!/usr/bin/env python
-
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-
 import cv2
 import numpy as np
 
-import sys
-
-#sys.path.insert(0, "/home/enzoc/.local/lib/python3.6/site-packages/tensorflow")
-
-#import tensorflow as tf
-#from tensorflow.keras.models import load_model
-
-bridge = CvBridge()
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 path = './sentiment_analysis/miniXception.h5'
 
-#new_model = load_model(path)
+new_model = load_model(path)
 
-#new_model.summary()
+new_model.summary()
 
-def image_callback(ros_image):
+def camera2Recv():
 
     emo_dict = {0: 'happy', 1: 'sad', 2: 'surprise', 3: 'fear', 4: 'disgust-contempt', 5: 'anger'}
 
-    global bridge
-
-    try:
-        cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
-    except CvBridgeError as e:
-        print(e)
+    cap = cv2.VideoCapture(0)
 
     width = 150
     height = 150
-
-    cv2.imshow('Frame', cv_image)
-    cv2.waitKey(1)
   
-'''
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
@@ -54,23 +33,17 @@ def image_callback(ros_image):
 
             print('The sentiment detect is: ' + emo_dict[sentimiento])
 
+            font = cv2.FONT_HERSHEY_PLAIN
+            cv2.putText(frame, emo_dict[sentimiento], (220,440), font, 3, (255,255,255), 2, cv2.LINE_AA)
             cv2.imshow('Frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
-    cap.release()'''
-
-def camera2Recv():
-
-    rospy.init_node('sentiment_analysis_img', anonymous=True)
-    sub_image = rospy.Subscriber("/usb_cam/image_raw", Image, image_callback)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+    cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 
+    print("Extrae imagen de la cámara PTZ")
     camera2Recv()
