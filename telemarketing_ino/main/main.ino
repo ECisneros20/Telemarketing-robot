cdaabf275ba3d5b119155388b1ac874794511ecc
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-01-22 22:13:50 -0500

    0.0.3

--- telemarketing_ino/main/main.ino
+++ telemarketing_ino/main/main.ino
@@ -1,11 +1,10 @@
-
-
-const int US[8]={A0,A1,A2,A3,A4,A5,A6,A7}; //Sonares
-const int IR[8]={A8,A9,A10,A11,A12,A13,A14,A15}; //Infrarojos
-
+// Ultrasonic sensors
+const int US[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
+// Infrared sensors
+const int IR[8] = {A8, A9, A10, A11, A12, A13, A14, A15};
 
 void setup() {
-  for(int i=0;i<=7;i++){
+  for(int i = 0; i <= 7; i++){
     pinMode(US[i],INPUT);
     pinMode(IR[i],INPUT);
   }
@@ -14,8 +13,9 @@ void setup() {
 void loop() {
 }
 
-float read_IR(int sensor){
-  float raw=0,distance=0;
+float read_IR(int sensor) {
+  float raw = 0,distance=0;
+  
   for(int i=0;i<10;i++){
     raw=analogRead(IR[sensor]);
     distance+=13*pow(raw*0.0048828125,-1);
