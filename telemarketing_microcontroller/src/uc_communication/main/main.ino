b0069c7ba6effaba506921433f9fa59f6d07a465
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-02-11 17:23:21 -0500

    0.1.0

--- telemarketing_microcontroller/src/uc_communication/main/main.ino
+++ telemarketing_microcontroller/src/uc_communication/main/main.ino
@@ -4,47 +4,47 @@ const int US[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
 const int IR[8] = {A8, A9, A10, A11, A12, A13, A14, A15};
 
 void setup() {
-  Serial.begin(9600);
-  for(int i = 0; i <= 7; i++){
-    pinMode(US[i],INPUT);
-    pinMode(IR[i],INPUT);
-  }
+    Serial.begin(9600);
+    for (int i=0; i<=7; i++) {
+        pinMode(US[i],INPUT);
+        pinMode(IR[i],INPUT);
+    }
 }
 
 void loop() {
-  
-  for(int i=0;i<=7;i++){
-    Serial.print(" | Sonar"+String(i+1)+": ");
-    Serial.print(String(read_US(i))+"cm");
+    for (int i=0; i<=7; i++) {
+        Serial.print(" | Sonar"+String(i+1)+": ");
+        Serial.print(String(read_US(i))+"cm");
     }
-  Serial.println();
+    Serial.println();
 
-  
-  for(int i=0;i<=7;i++){
-    Serial.print(" | IR"+String(i+1)+": ");
-    Serial.print(String(read_IR(i))+"cm");
+    for (int i=0; i<=7; i++) {
+        Serial.print(" | IR"+String(i+1)+": ");
+        Serial.print(String(read_IR(i))+"cm");
     }
-  Serial.println();  
+    Serial.println();
 }
 
 float read_IR(int sensor) {
-  float raw = 0,distance=0;
-  
-  for(int i=0;i<10;i++){
-    raw=analogRead(IR[sensor]);
-    distance+=13*pow(raw*0.0048828125,-1);
-    delay(10);
-  }
-  return distance/10;
+    float raw = 0, distance = 0;
+
+    for(int i=0; i<10; i++) {
+        raw = analogRead(IR[sensor]);
+        distance += 13*pow(raw*0.0048828125,-1);
+        delay(10);
+    }
+
+    return distance/10;
 }
 
-float read_US(int sensor){
-  float raw=0,distance=0;
-   
-  for(int i=0;i<10;i++){
-    raw=analogRead(US[sensor]);
-    distance += raw*0.5;
-    delay(10);
-  }
-  return distance/10;
+float read_US(int sensor) {
+    float raw = 0, distance = 0;
+
+    for(int i=0; i<10; i++) {
+        raw = analogRead(US[sensor]);
+        distance += raw*0.5;
+        delay(10);
+    }
+
+    return distance/10;
 }
