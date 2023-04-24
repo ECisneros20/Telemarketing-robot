44b4bc617255101a9ca477e6f7a929a2ea3e7743
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-04-24 05:33:58 -0500

    Several updates - 0.3.0

---
+++ telemarketing_microcontroller/src/Controller.py
@@ -0,0 +1,44 @@
+from simple_pid import PID
+import time
+
+class MotorController:
+    def __init__(self):
+      self.TICKS_PER_REVOLUTION = 620 #Calculate this value manually
+      self.TICKS_PER_METER = 3100 #Number of ticks a wheel makes moving a linear distance of 1 meter
+      self.prevLeftCount=0
+      self.prevTimeLeft=0      
+      self.prevRightCount=0
+      self.prevTimeRight=0
+
+    def calculate_vel_left_wheel(self, left_ticks):
+      numOfTicks = (65535 + left_ticks - self.prevLeftCount) % 65535
+      if (numOfTicks > 10000):#Whenever the counter pass from positive to negative values
+        numOfTicks = 0 - (65535 - numOfTicks)
+      self.velLeftWheel = numOfTicks/self.TICKS_PER_METER/((time.time())-self.prevTimeLeft)
+
+      self.prevLeftCount=left_ticks
+      self.prevTimeLeft=time.time()
+      
+    
+    def calculate_vel_right_wheel(self, right_ticks):
+      numOfTicks = (65535 + right_ticks - self.prevRightCount) % 65535
+      if (numOfTicks > 10000):
+        numOfTicks = 0 - (65535 - numOfTicks)
+      self.velRightWheel = numOfTicks/self.TICKS_PER_METER/((time.time())-self.prevTimeRight)
+
+      self.prevRightCount=right_ticks
+      self.prevTimeRight=time.time()
+
+       
+    def calculate_pwm_values(self, desiredSpeed_left, desiredSpeed_right, left_ticks, right_ticks):
+      self.calculate_vel_left_wheel(left_ticks, right_ticks)
+      Kp_left=1;Ki_left=1;Kd_left=1
+      Kp_right=1;Ki_right=1;Kd_right=1
+      pid_left = PID(Kp_left, Ki_left, Kd_left, setpoint=desiredSpeed_left)
+      pid_right = PID(Kp_right, Ki_right, Kd_right, setpoint=desiredSpeed_right)
+      pid_left.output_limits = (-255, 255);pid_right.output_limits = (-255, 255)
+      pid_left.sample_time = 0.001;pid_right.sample_time = 0.001
+
+      PWM_Output_left = pid_left(self.velLeftWheel)
+      PWM_Output_right = pid_right(self.velRightWheel)
+      return PWM_Output_left, PWM_Output_right
--- telemarketing_microcontroller/src/ROBOTEQ.py
+++ telemarketing_microcontroller/src/ROBOTEQ.py
@@ -1,50 +1,51 @@
 import serial, time
 
-serialPort = 'COM17' #cambiar
+class roboteq:
+    
+    def __init__(self, serialPort = 'COM17'):
+        self.voltage=0
+        self.roboteq = serial.Serial(
+            port=serialPort,    
+            baudrate=9600,         
+            bytesize=serial.SEVENBITS, 
+            parity=serial.PARITY_EVEN,  
+            stopbits=serial.STOPBITS_ONE)
+        time.sleep(2)
 
-roboteq = serial.Serial(
-    port=serialPort,
-    baudrate=9600,
-    bytesize=serial.SEVENBITS,
-    parity=serial.PARITY_EVEN,
-    stopbits=serial.STOPBITS_ONE)
+    def ReadBattery(self):
+        self.roboteq.write(b'?E\r\n')
+        self.roboteq.read(3) #echo
+        raw=self.roboteq.read(2) 
+        raw=int(raw,16)
+        self.roboteq.close() 
+        self.roboteq.open()
+        self.voltage=55*raw/256
+        self.voltage=round(self.voltage,2)
+        return self.voltage
 
-time.sleep(2)
-
-def ReadBattery():
-    roboteq.write(b'?E\r\n')
-    roboteq.read(3) #echo
-    raw=roboteq.read(2)
-    raw=int(raw,16)
-    roboteq.close()
-    roboteq.open()
-    voltage=55*raw/256
-    voltage=round(voltage,2)
-    return voltage
-
-def setMotors(motor,speed,clockwise=True):
-    if(not(clockwise)):
-        motor=motor.lower()
-    speed=speed*127/100
-    hex_num = hex(int(speed))
-    speedHex= '{:02X}'.format(int(hex_num, 16))
-    value='!'+motor+str(speedHex)
-    roboteq.write(value.encode())
-    roboteq.write(b'\r\n')
-    roboteq.close()
-    roboteq.open()
+    def setMotors(self, motor,speed,clockwise=True):
+        if(not(clockwise)):
+            motor=motor.lower()
+        speed=speed*127/100
+        hex_num = hex(int(speed))
+        speedHex= '{:02X}'.format(int(hex_num, 16))
+        value='!'+motor+str(speedHex)
+        self.roboteq.write(value.encode())
+        self.roboteq.write(b'\r\n')
+        self.roboteq.close()
+        self.roboteq.open()
 
 while(True):
+    driver = roboteq(serialPort="COM17")
     option=input("¿Qué deseas hacer? (M o B): ")
     if option=='M':
         while(True):
-            raw_motor=input("Ingresa el motor y velocidad (A100):")
+            raw_motor=input("Ingresa el motor y velocidad (A100):") 
             if raw_motor=="0":
                 break
-            setMotors(raw_motor[0],int(raw_motor[1:]),True)
+            driver.setMotors(raw_motor[0],int(raw_motor[1:]),True)
     elif option=='B':
-        voltage=ReadBattery()
+        voltage=driver.ReadBattery()
         print(f"El voltaje de las baterías es {voltage}v")
     else:
         print("Error.")
-        #roboteq.close()
