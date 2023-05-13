24247aa5a853e90d7a7ff88751dceac6446acf40
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-05-13 02:17:51 -0500

    Update of description and microcontroller packages - 0.3.4

--- telemarketing_microcontroller/src/serial.ino
+++ telemarketing_microcontroller/src/serial.ino
@@ -1,83 +1,94 @@
 #include <ros.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <Servo.h>
+
 // Infrared sensors
 const int IR[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 // Ultrasonic sensors
 const int US[8] = {8, 9, 10, 11, 12, 13, 14, 15};
 // Encoder ticks
-volatile int ticksR=0;
-volatile int ticksL=0;
-const int encoderR[2]={3,2}; //{channelA, channelB}
-const int encoderL[2]={19,18}; //{channelA, channelB}
+volatile int ticksR = 0;
+volatile int ticksL = 0;
+const int encoderR[2] = {3, 2}; // {channelA, channelB}
+const int encoderL[2] = {19, 18}; // {channelA, channelB}
 const int encoder_minimum = -32768;
 const int encoder_maximum = 32767;
 // Array of 16 sensors + bumpers
 float sensor_data[19];
 
-//Motors
+// Motors
 Servo MotorR;
 Servo MotorL;
 
-volatile unsigned long TimeBackup=0;
+volatile unsigned long TimeBackup = 0;
 ros::NodeHandle nh;
 
 std_msgs::Float32MultiArray msg;
 ros::Publisher pub("/sensor_data", &msg);
 
-void setup()
-{ nh.initNode();
-  nh.advertise(pub);
-  for(int i=0;i<=1;i++){
-    pinMode(encoderR[i],INPUT);
-    pinMode(encoderL[i],INPUT);}
-  MotorR.attach(5);
-  MotorL.attach(6);
-  attachInterrupt(digitalPinToInterrupt(encoderL[0]), encoderLcallback, FALLING);
-  attachInterrupt(digitalPinToInterrupt(encoderR[0]), encoderRcallback, FALLING);
+void setup() {
+
+    nh.initNode();
+    nh.advertise(pub);
+
+    for (int i=0; i<=1; i++) {
+        pinMode(encoderR[i], INPUT);
+        pinMode(encoderL[i], INPUT);
+    }
+
+    MotorR.attach(5);
+    MotorL.attach(6);
+    attachInterrupt(digitalPinToInterrupt(encoderL[0]), encoderLcallback, FALLING);
+    attachInterrupt(digitalPinToInterrupt(encoderR[0]), encoderRcallback, FALLING);
+
 }
 
-void loop()
-{ for (int i=0; i<=7; i++) {
-      sensor_data[i] = read_US(i);
-  }
+void loop() {
 
-  for (int i=8; i<=15; i++) {
-      sensor_data[i] = read_IR(i);
-  }
-  if(millis()-TimeBackup>=100){ //100 ms  
-    
-    sensor_data[16]=ticksL;
-    sensor_data[17]=ticksR;
-    
-    // Set the message data
-    msg.data_length = 18;
-    msg.data = sensor_data;
-  
-    // Publish the message
-    pub.publish(&msg);
-    nh.spinOnce();
-    TimeBackup=millis();
-  }
-  
+    for (int i=0; i<=7; i++) {
+        sensor_data[i] = read_US(i);
+    }
+
+    for (int i=8; i<=15; i++) {
+        sensor_data[i] = read_IR(i);
+    }
+
+    if (millis()-TimeBackup>=100) { //100 ms
+
+        sensor_data[16] = ticksL;
+        sensor_data[17] = ticksR;
+
+        // Set the message data
+        msg.data_length = 18;
+        msg.data = sensor_data;
+
+        // Publish the message
+        pub.publish(&msg);
+        nh.spinOnce();
+        TimeBackup = millis();
+    }
+
 }
 
 float read_IR(int sensor) {
+
     float raw = 0, distance = 0;
 
-    for(int i=0; i<10; i++) {
+    for (int i=0; i<10; i++) {
         raw = analogRead(IR[sensor]);
         distance += 13*pow(raw*0.0048828125,-1);
         delay(10);
     }
 
     return distance/1000;
+
 }
 
 float read_US(int sensor) {
+
     float raw = 0, distance = 0;
 
-    for(int i=0; i<10; i++) {
+    for (int i=0; i<10; i++) {
         raw = analogRead(US[sensor]);
         distance += raw*0.5;
         delay(10);
@@ -86,48 +97,48 @@ float read_US(int sensor) {
     return distance/1000;
 }
 
-void encoderLcallback(){
-  if (digitalRead(encoderL[0]) != digitalRead(encoderL[1])){ 
-    if (ticksL == encoder_maximum) {
-      ticksL = encoder_minimum;
+void encoderLcallback() {
+
+    if (digitalRead(encoderL[0]) != digitalRead(encoderL[1])) {
+        if (ticksL == encoder_maximum)
+            ticksL = encoder_minimum;
+        else
+            ticksL++;
     }
     else {
-      ticksL++;  
+        if (ticksL == encoder_minimum)
+            ticksL = encoder_maximum;
+        else
+            ticksL--;
     }
-    }
-  else{
-    if (ticksL == encoder_minimum) {
-      ticksL = encoder_maximum;
-    }
-    else {
-      ticksL--;  
-    }
-    } 
+
 }
 
-void encoderRcallback(){
-  if (digitalRead(encoderR[0]) == digitalRead(encoderR[1])){ 
-    if (ticksR == encoder_maximum) {
-      ticksR = encoder_minimum;
+void encoderRcallback() {
+
+    if (digitalRead(encoderR[0]) == digitalRead(encoderR[1])) {
+        if (ticksR == encoder_maximum)
+            ticksR = encoder_minimum;
+        else
+            ticksR++;
     }
     else {
-      ticksR++;  
+        if (ticksR == encoder_minimum)
+            ticksR = encoder_maximum;
+        else
+            ticksR--;
     }
-    }
-  else{
-    if (ticksR == encoder_minimum) {
-      ticksR = encoder_maximum;
-    }
-    else {
-      ticksR--;  
-    }
-    } 
+
 }
 
 void setMotorR(int pulse){
-  MotorR.writeMicroseconds(pulse);
+
+    MotorR.writeMicroseconds(pulse);
+
 }
 
 void setMotorL(int pulse){
-  MotorL.writeMicroseconds(pulse);
+
+    MotorL.writeMicroseconds(pulse);
+
 }
