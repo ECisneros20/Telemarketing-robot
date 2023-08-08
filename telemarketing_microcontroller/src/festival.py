a7efbab36b9fb1348fb20a7e99bcad35ed3f018b
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-08-08 16:38:04 -0500

    Update festival.py script - 0.3.9

--- telemarketing_microcontroller/src/festival.py
+++ telemarketing_microcontroller/src/festival.py
@@ -38,7 +38,7 @@ class SerialComSensors:
         # max angular z = 1.5708 rad/s
         self.v_r = -(2 * msg.linear.x + msg.angular.z * self.L) / (2 * self.R)
         self.v_l = (2 * msg.linear.x - msg.angular.z * self.L) / (2 * self.R)
-        self.servo_msg.data = {self.v_r, self.v_l}
+        self.servo_msg.data = {int(30.48 * self.v_r + 1500), int(30.48 * self.v_l + 1500)}
 
     def publisherFunctions(self):
 
