f5257bcac78fb5d23e86837486791059c01a5a2b
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-08-10 19:37:24 -0500

    Update festival-teleop.py script - 0.3.9

--- telemarketing_microcontroller/src/festival-teleop.py
+++ telemarketing_microcontroller/src/festival-teleop.py
@@ -54,6 +54,7 @@ if __name__ == "__main__":
         # Node initialization
         com = SerialComTeleop()
         com.publisherFunctions()
+        rospy.spin()
 
     except rospy.ROSInterruptException:
         pass
