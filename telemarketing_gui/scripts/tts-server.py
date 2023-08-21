d45956a14f6c6ca92ae26a8e3294a39606549cc7
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2023-08-21 02:54:55 -0500

    Add ROS server and client scripts with srv file - 0.3.10

--- telemarketing_gui/scripts/tts-server.py
+++ telemarketing_gui/scripts/tts-server.py
@@ -8,6 +8,7 @@ import sounddevice as sd
 import soundfile as sf
 import torch
 import scipy
+# Link: https://www.youtube.com/watch?v=6RrcQFmNO58
 # Link: https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
 
 # Server (1)
