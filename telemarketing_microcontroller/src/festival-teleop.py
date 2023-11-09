6d66000f694d1c145e93ee6e0b6d03f5417ed6ed
MAlvites <malvites1602@gmail.com>
2023-11-09 17:36:14 -0500

    festival-teleop.py fixed

--- telemarketing_microcontroller/src/festival-teleop.py
+++ telemarketing_microcontroller/src/festival-teleop.py
@@ -25,9 +25,8 @@ class SerialComTeleop:
         rospy.init_node("serial_com_teleop_node")
         self.sub_teleop = rospy.Subscriber("/cmd_vel", Twist, self.callback_teleop)
         self.pub_servo_vel = rospy.Publisher("/servo_vel", Int32MultiArray, queue_size = 10)
-        self.pub_vel_setpoint = rospy.Publisher("/vel_setpoint", Float32MultiArray, queue_size = 10)
+        #self.pub_vel_setpoint = rospy.Publisher("/vel_setpoint", Float32MultiArray, queue_size = 10)
         self.servo_msg = Int32MultiArray(data = [1500, 1500])
-        self.setpoint_msg = Float32MultiArray(data = [0, 0])
         self.rate = rospy.Rate(10)
 
     def callback_teleop(self, msg):
@@ -38,18 +37,15 @@ class SerialComTeleop:
         self.v_r = -(2 * msg.linear.x + msg.angular.z * self.L) / (2 * self.R)
         self.v_l = (2 * msg.linear.x - msg.angular.z * self.L) / (2 * self.R)
         self.servo_msg.data = [int(50.80 * self.v_r + 1500), int(50.80 * self.v_l + 1500)]
-        self.setpoint_msg.data = [self.v_r, self.v_l]
 
     def publisherFunctions(self):
 
         while not rospy.is_shutdown():
             self.pub_servo_vel.publish(self.servo_msg)
-            self.pub_vel_setpoint.publish(self.setpoint_msg)
-            rospy.loginfo("Executing!")
+            rospy.loginfo(self.servo_msg)
             self.rate.sleep()
 
-        self.pub_servo_vel.publish(Int32MultiArray(data = [1500, 1500]))
-        self.pub_vel_setpoint.publish(Float32MultiArray(data = [0, 0]))
+        #self.pub_servo_vel.publish(Int32MultiArray(data = [1500, 1500]))
 
 
 if __name__ == "__main__":
